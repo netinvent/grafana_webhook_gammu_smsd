3041ba0f427db71ca483c02f2c37304f782be0c6
deajan <ozy@netpower.fr>
2026-01-29 23:41:57 +0100

    Update modem detection script

--- examples/systemd/gammu_detect_modem.sh
+++ examples/systemd/gammu_detect_modem.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-# This script updates /etc/gammu-smsdrc file with first detected modem
-# Written in 2022-2024 by Orsiris de Jong - NetInvent
-SCRIPTVER=2024030801
-
+# This script updates /etc/gammu-smsdrc file with first detected USB modem
+# Really quick and dirty (TM) 2022-2026 Orsiris de Jong - ozy [at] netpower dot fr
+SCRIPTVER=2026012901
 
 log_file="/var/log/modem_update.log"
 gammu_config_file="/etc/gammu-smsdrc"
 modem=""
 
-# gammu_srv
+# gammu service
 gammu_service="gammu-smsd"
 # Optional command when modem port change detected
-optional_cmd="systemctl restart ${gammu-smsd}"
+optional_cmd=""
 
 function log {
     line="${1}"
@@ -27,6 +26,7 @@ if ! type picocom > /dev/null 2>1; then
     exit 1
 fi
 
+# Stop gammu before trying to discuss with modem
 systemctl stop ${gammu_service}
 
 for tty in $(ls /dev/ttyUSB*); do
@@ -34,10 +34,12 @@ for tty in $(ls /dev/ttyUSB*); do
     picocom -qrX -b 9600 $tty
     sleep 1
     result=$(echo "AT&F" | picocom -qrix 1000 $tty)
-    if [ "${result}" = "AT&F" ]; then
+    case $result in
+        *"AT&F"*)
         log "Found AT compatible modem at $tty"
         modem="${tty}"
-    fi
+        ;;
+    esac
 done
 
 curr_gammu_modem=$(cat "${gammu_config_file}" | grep -e "^device = " | awk -F'=' '{gsub(/ /,""); print $2}')
@@ -45,14 +47,15 @@ if [ "${modem}" != "" ]; then
     if [ "${modem}" != "${curr_gammu_modem}" ]; then
         log "Updating modem from ${curr_gammu_modem} to ${modem}"
         sed -i "s%\#\?\device \?=.*%\device = ${modem}%g" "${gammu_config_file}"
-        #if [ "${optional_cmd}" != "" ]; then
-        #    log "Running ${optional_cmd}"
-        #    ${optional_cmd} >> "${log_file}"
-        #    if [ $? -ne 0 ]; then
-        #        log "Could not restart service, exitcode: $?"
-        #    fi
-        #fi
+        if [ "${optional_cmd}" != "" ]; then
+            log "Running ${optional_cmd}"
+            ${optional_cmd} >> "${log_file}" 2>&1
+            if [ $? -ne 0 ]; then
+                log "Optional command failed with exitcode: $?"
+            fi
+        fi
     fi
+
 else
     log "No usable modem found."
 fi
